fa51df391ec8d87fd4dca33ebd508bd8e188b41b
XFernando43 <U20171E997@upc.edu.pe>
2023-12-16 01:54:10 -0500

    add header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HeaderComponent from './components/shared/HeaderComponents'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-
-      <h1 className="text-red-700 font-bold text-3xl">HOLI</h1>
-
-    </div>
+    <>
+      <HeaderComponent></HeaderComponent>
+    </>
   )
 }
 
